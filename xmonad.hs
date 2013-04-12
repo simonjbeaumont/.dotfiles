eb6a0331758f20ec39740a6c96e94c0b4a383a0f
Si Beaumont <simon.beaumont@citrix.com>
2013-04-12 11:03:46 +0100

    Add Comminactor launch shortcut to xmonad.

--- xmonad.hs
+++ xmonad.hs
@@ -68,7 +68,8 @@ main = do
               -- launching
               , ((modMask' .|. shiftMask, xK_b),  spawn "google-chrome")
               , ((modMask' .|. shiftMask, xK_s),  spawn "/home/simonbe/spotify-notify/spotify-notify.py -n")
-              , ((modMask' .|. shiftMask, xK_F5), spawn "/opt/Citrix/ICAClient/wfica /local/config/XenApp-simonbe.ica")
+              , ((modMask' .|. shiftMask, xK_F5), spawn "/opt/Citrix/ICAClient/wfica /local/config/XenApp-Outlook-simonbe.ica")
+              , ((modMask' .|. shiftMask, xK_F6), spawn "/opt/Citrix/ICAClient/wfica /local/config/XenApp-Communicator-simonbe.ica")
               , ((modMask' .|. shiftMask, xK_p),  spawn "gmrun")
               , ((modMask',               xK_p),  spawn "gnome-do")
               ]
