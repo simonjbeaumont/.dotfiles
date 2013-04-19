6d7d478fb67ce3fee4af9e11941d63d4a1981f6f
Si Beaumont <simon.beaumont@citrix.com>
2013-04-19 17:54:54 +0100

    tmux updates digest

--- tmux/tmux-powerline/my-themes/my-theme.sh
+++ tmux/tmux-powerline/my-themes/my-theme.sh
@@ -23,11 +23,11 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		#"hostname 33 0" \
+		"hostname 148 234" \
+		"tmux_session_info 24 255" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-        "lan_ip 24 255" \
+		#"lan_ip 24 255" \
 		#"wan_ip 24 255" \
 		# "vcs_branch 29 88" \
 		# "vcs_compare 60 255" \
