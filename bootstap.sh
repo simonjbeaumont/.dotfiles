6fad9dffa6265bec129aaca0c954f40c58bbdef5
Si Beaumont <simon.beaumont@citrix.com>
2015-06-23 12:19:01 +0100

    vimperator: Add config

--- bootstap.sh
+++ bootstap.sh
@@ -49,6 +49,7 @@ install_dotfile "$HOME/.dotfiles/tmux/tmux-powerline/tmux-powerlinerc" "$HOME/.t
 install_dotfile "$HOME/.dotfiles/tmux/tmux_start" "$HOME/tmux_start"
 install_dotfile "$HOME/.dotfiles/vim" "$HOME/.vim"
 install_dotfile "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
+install_dotfile "$HOME/.dotfiles/vimperatorrc" "$HOME/.vimperatorrc"
 
 # OS specific config
 case "$OSTYPE" in
