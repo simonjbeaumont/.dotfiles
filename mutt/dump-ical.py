0660e7974baad33dbdf7e167310076cc25f111b3
Florian Klink <flokli@flokli.de>
2016-08-24 12:32:26 +0200

    mutt: let dump-ical use string format method

--- mutt/dump-ical.py
+++ mutt/dump-ical.py
@@ -17,13 +17,13 @@ def get_invitation_from_path(path):
 
 
 def person_string(c):
-    return "%s %s" % (c.params['CN'][0], "<%s>" % c.value.split(':')[1])
+    return "{} {}".format(c.params['CN'][0], "<%s>" % c.value.split(':')[1])
 
 
 def when_str_of_start_end(s, e):
     date_format = "%a, %d %b %Y at %H:%M"
     until_format = "%H:%M" if s.date() == e.date() else date_format
-    return "%s -- %s" % (s.strftime(date_format), e.strftime(until_format))
+    return "{} -- {}".format(s.strftime(date_format), e.strftime(until_format))
 
 
 def pretty_print_invitation(invitation):
@@ -36,25 +36,25 @@ def pretty_print_invitation(invitation):
     location = event['location'][0].value
     description = event['description'][0].value
     sequence = event['sequence'][0].value
-    print "="*70
+    print("="*70)
     if int(sequence) > 0:
-        print "MEETING UPDATE".center(70)
+        print("MEETING UPDATE".center(70))
     else:
-        print "MEETING INVITATION".center(70)
-    print "="*70
-    print "Event:\n\t%s" % title.encode('utf-8')
-    print "Organiser:\n\t%s" % person_string(org).encode('utf-8')
-    print "Invitees:"
+        print("MEETING INVITATION".center(70))
+    print("="*70)
+    print("Event:\n\t{}".format(title))
+    print("Organiser:\n\t{}".format(person_string(org)))
+    print("Invitees:")
     for i in invitees:
-        print "\t%s" % person_string(i).encode('utf-8')
-    print "When:\n\t%s" % when_str_of_start_end(start, end).encode('utf-8')
-    print "Location:\n\t%s" % location.encode('utf-8')
-    print "---\n%s---" % description.encode('utf-8')
+        print("\t{}".format(person_string(i)))
+    print("When:\n\t{}".format(when_str_of_start_end(start, end)))
+    print("Location:\n\t{}".format(location))
+    print("---\n{}---".format(description))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or sys.argv[1].startswith('-'):
-        sys.stderr.write("Usage: %s <filename.ics>\n" % sys.argv[0])
+        sys.stderr.write("Usage: %s <filename.ics>\n".format(sys.argv[0]))
         sys.exit(2)
     inv = get_invitation_from_path(sys.argv[1])
     pretty_print_invitation(inv)
