8c6012311b55d249a6f1d80ee3ad7e8b8e43b766
Si Beaumont <simonjbeaumont@gmail.com>
2016-12-14 14:06:31 +0000

    mutt: Use wrapper script for ldap search
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

---
+++ mutt/pull-ldap-wrapper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FILTER="$1"
+
+main () {
+  people=$(python ~/.mutt/pull-ldap.py --host @@HOST@@ --base '@@BASE_PEOPLE@@' --extra-fields @@EXTRA_FIELDS_PEOPLE@@ --filter "${FILTER}" 2>/dev/null) \
+  && \
+  groups=$(python ~/.mutt/pull-ldap.py --host @@HOST@@ --base '@@BASE_GROUPS@@' --extra-fields @@EXTRA_FIELDS_GROUPS@@ --filter "${FILTER}" 2>/dev/null)
+
+  if [[ $? == 4 ]]; then
+    echo "--"
+    echo "too-many-results"
+    echo "--"
+  fi
+
+  if [[ "${people}" != "" ]] && [[ "${groups}" != "" ]]; then
+    echo "-- People:"
+    echo "${people}"
+    echo "-- Groups:"
+    echo "${groups}"
+  elif [[ "${people}" != "" ]]; then
+    echo "${people}"
+  elif [[ "${groups}" != "" ]]; then
+    echo "${groups}"
+  fi
+}
+
+main
