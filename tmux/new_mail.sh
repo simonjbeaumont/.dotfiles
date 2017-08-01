bf150a808526790e522c70872c08d4cc662d74fb
Si Beaumont <simonjbeaumont@gmail.com>
2017-08-01 19:31:07 +0100

    tmux: Update status bar with VIP and Github email counts
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

--- tmux/new_mail.sh
+++ tmux/new_mail.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-count=$(ls /Users/Si/work/mail/REMOTE/Inbox/new | wc -l)
+inbox=$(notmuch search "path:Inbox/new" | wc -l)
 
-if [[ -n "$count"  && "$count" -gt 0 ]]; then
-  echo "┃ ✉ ${count} "
+vip=$(notmuch search "path:Inbox/new AND (from:mdinacci OR from:blaisethom OR from:drummie)" | wc -l)
+
+github=$(notmuch search "path:github/new" | wc -l)
+
+if [[ ${inbox} -gt 0 || ${vip} -gt 0 || ${github} -gt 0 ]]; then
+  echo "┃ ✉ i:${inbox} !:${vip} g:${github} "
 fi
