01403796ecc7f1e169ed796703396e5acf53f9b1
Si Beaumont <simonjbeaumont@gmail.com>
2017-11-20 14:48:22 +0000

    tmux: Remove VIP email from status bar
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

--- tmux/new_mail.sh
+++ tmux/new_mail.sh
@@ -2,10 +2,10 @@
 
 inbox=$(notmuch search "path:Inbox/new" | wc -l)
 
-vip=$(notmuch search "path:Inbox/new AND (from:mdinacci OR from:blaisethom OR from:drummie)" | wc -l)
+team=$(notmuch search "path:lists.siri-vector-platform-team/new" | wc -l)
 
 github=$(notmuch search "path:github/new" | wc -l)
 
-if [[ ${inbox} -gt 0 || ${vip} -gt 0 || ${github} -gt 0 ]]; then
-  echo "┃ ✉ i:${inbox} !:${vip} g:${github} "
+if [[ ${inbox} -gt 0 || ${github} -gt 0 ]]; then
+  echo "┃ ✉ i:${inbox} t:${team} g:${github} "
 fi
