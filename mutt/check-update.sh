ec2b0f10a8a3dade2cf31990ade5d407092dce94
Si Beaumont <beaumont@apple.com>
2018-02-05 11:14:49 +0000

    mutt: Use longer timeout for mailing list update
    Signed-off-by: Si Beaumont <beaumont@apple.com>

--- mutt/check-update.sh
+++ mutt/check-update.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set -eou pipefail
+
 echo $(date) $0 $@
-/usr/local/opt/coreutils/libexec/gnubin/timeout 1m /usr/local/bin/mbsync -c /Users/Si/.mbsyncrc $1
+
+case "$1" in
+  inbox) TIMEOUT=1m ;;
+  other) TIMEOUT=5m ;;
+esac
+
+/usr/local/opt/coreutils/libexec/gnubin/timeout ${TIMEOUT} /usr/local/bin/mbsync -c /Users/Si/.mbsyncrc $1 || echo "TIMEOUT"
+
 /usr/local/bin/notmuch new
+
+echo $(date) $0 $@ done
