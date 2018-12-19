728250e136ecc0b796fa941d747eaf770bc10ee5
Si Beaumont <beaumont@apple.com>
2018-12-19 11:31:24 +0000

    mutt: Adjust new mail notification for new team
    Signed-off-by: Si Beaumont <beaumont@apple.com>

--- mutt/inbox_bell.sh
+++ mutt/inbox_bell.sh
@@ -4,19 +4,23 @@ set -e
 NOTIFICATION_CACHE=/var/tmp/mutt-notification
 
 inbox=$(notmuch search "path:Inbox/new" | wc -l)
-team=$(notmuch search "path:lists.siri-vector-platform-team/new" | wc -l)
+team=$(notmuch search "path:lists/superbuild-dev/new" | wc -l)
 github=$(notmuch search "path:github/new" | wc -l)
 
 old_notification=$(cat ${NOTIFICATION_CACHE} 2>/dev/null || true)
-new_notification="i:${inbox} !:${vip} t:${team} g:${github}"
+new_notification="i:${inbox} t:${team} g:${github}"
 echo ${new_notification} > ${NOTIFICATION_CACHE}
+
+if [ $(( ${inbox} + ${team} + ${github} )) -eq 0 ]; then
+  exit 0
+fi
 
 if [ "${new_notification}" == "${old_notification}" ]; then
   exit 0
 fi
 
 title="📫 New Mail"
-message="Inbox: ${inbox}   VIP: ${vip}   Team: ${team}   Github: ${github}"
+message="Inbox: ${inbox}   Team: ${team}   Github: ${github}"
 
 /usr/bin/osascript -e "display notification \"${message}\" with title \"${title}\""
 echo -en "\a"
