687ab112c26d2313ea432429537d8a38f4f12690
Si Beaumont <simonjbeaumont@gmail.com>
2021-02-08 15:54:43 +0000

    tmux: Use longer timeout in VPN status script
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

--- tmux/vpn_status.sh
+++ tmux/vpn_status.sh
@@ -2,6 +2,6 @@
 
 TEST_HOST=mail.apple.com
 
-if ! timeout 1 ping -qc1 ${TEST_HOST} >/dev/null 2>&1; then
+if ! timeout 3 ping -qc1 ${TEST_HOST} >/dev/null 2>&1; then
   echo " VPN: NOT CONNECTED "
 fi
