5075c30a33e36f25f8f544eca611ef3fac6c9db3
Si Beaumont <simonjbeaumont@gmail.com>
2021-02-08 15:39:11 +0000

    mutt: Improve update script with support for internal Homebrew
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

--- mutt/check-update.sh
+++ mutt/check-update.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 set -eou pipefail
 
-echo $(date) $0 $@
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+trap 'error "command failed (line ${LINENO})"' ERR
+
+RUN_ID=$(uuidgen)
+
+log "Starting run ${RUN_ID} at $(date -u +"%Y-%m-%dT%H:%M:%SZ"): $0 $@"
+
+
+# The tools used are probably installed by Homebrew, need to set the PATH
+[ -r "/usr/local/bin" ] && export PATH="/usr/local/bin:$PATH"
+[ -r "/opt/brew/bin" ] && export PATH="/opt/brew/bin:$PATH"
+which -s brew && export HOMEBREW_PREFIX=$(dirname $(dirname $(which brew)))
+[ -r "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin" ] && export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
+
+which -s mbsync || fatal "Required executable not found: mbsync"
+which -s notmuch || fatal "Required executable not found: notmuch"
+which -s timeout || fatal "Required executable not found: timeout"
 
 case "$1" in
   inbox) TIMEOUT=1m ;;
   other) TIMEOUT=5m ;;
 esac
 
-/usr/local/opt/coreutils/libexec/gnubin/timeout ${TIMEOUT} /usr/local/bin/mbsync -c /Users/Si/.mbsyncrc $1 || echo "TIMEOUT"
+timeout ${TIMEOUT} mbsync -c /Users/Si/.mbsyncrc $1 || fatal "Failed to check for mail"
+notmuch new || fatal "Failed to run notmuch"
 
-/usr/local/bin/notmuch new
-
-echo $(date) $0 $@ done
+log "Completed run ${RUN_ID} at $(date -u +"%Y-%m-%dT%H:%M:%SZ")"
