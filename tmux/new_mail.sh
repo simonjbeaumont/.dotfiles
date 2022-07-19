233b9ecd207ec2d3983beecc1ac2d66b93934411
Si Beaumont <simonjbeaumont@gmail.com>
2022-07-19 10:09:58 +0100

    tmux: Simplify mail indicator in status bar
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

--- tmux/new_mail.sh
+++ tmux/new_mail.sh
@@ -2,10 +2,16 @@
 
 inbox=$(notmuch search "path:Inbox/new" | wc -l)
 
-github=$(notmuch search "path:github/new" | wc -l)
+# github=$(notmuch search "path:github/new" | wc -l)
+github=0
 
 if [ $(( ${inbox} + ${github} )) -eq 0 ]; then
   exit 0
 fi
 
-echo "✉ i:${inbox} g:${github}"
+# echo "✉ i:${inbox} g:${github}"
+echo "✉ ${inbox}"
+# echo "✉️  ${inbox}"
+# echo "📥 ${inbox}"
+# echo "📨 ${inbox}"
+# echo "📬 ${inbox}"
