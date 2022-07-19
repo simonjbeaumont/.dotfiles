2b31ff011354b3545c7776e2c3edb9de2d97f63d
Si Beaumont <simonjbeaumont@gmail.com>
2022-07-19 13:17:22 +0100

    mutt: Remove wrapper script for address completion
    Signed-off-by: Si Beaumont <simonjbeaumont@gmail.com>

--- mutt/pull-ldap.py
+++ mutt/pull-ldap.py
@@ -3,6 +3,7 @@
 import base64
 import subprocess
 import argparse
+import json
 import sys
 
 FIELD_MAIL = "mail"
@@ -17,19 +18,42 @@ ARG_FORMAT_VIM = "vim"
 
 
 def parse_args_or_exit(argv=None):
+    conf_parser = argparse.ArgumentParser(
+        description="Query LDAP server for mutt-compliant complete command",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False)
+    conf_parser.add_argument("-c", "--config",
+                             help="Path to config file", metavar="FILE")
+
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    # defaults
+    config = {
+        "extra_fields": [],
+        "mock_input": None,
+    }
+
+    if args.config:
+        with open(args.config) as f:
+            config.update(json.load(f))
+
     parser = argparse.ArgumentParser(
-        description="Query LDAP server for mutt-compliant complete command")
-    parser.add_argument("--host", required=True,
+        description="Query LDAP server for mutt-compliant complete command",
+        parents=[conf_parser])
+    parser.set_defaults(**config)
+    parser.add_argument("--host", required=("host" not in config),
                         help="Address of LDAP server")
-    parser.add_argument("--base", required=True,
-                        help="Starting point for the search")
+    parser.add_argument("--base-people", required=("base_people" not in config),
+                        help="Starting point for the person search")
+    parser.add_argument("--base-groups", required=("base_groups" not in config),
+                        help="Starting point for the group search")
     parser.add_argument("--sortby", default="givenName",
                         help="Sort by this field (default: givenName)")
-    parser.add_argument("--extra-fields", nargs="+", default=[],
+    parser.add_argument("--extra-fields", nargs="+",
                         help="Extra fields to search and display")
     parser.add_argument("--filter", required=True,
                         help="Search term (part of name)")
-    parser.add_argument("--mock-input", default=None,
+    parser.add_argument("--mock-input",
                         help="Use file input instead of server (default: None)")
     parser.add_argument("--dry-run", action="store_true",
                         help="Just print ldapsearch command but don't execute")
@@ -53,9 +77,12 @@ def ldapsearch(host, base, sortby, filter, fields, dry_run=False):
         print(' '.join(["\"{}\"".format(x) for x in ldap_cmd]))
         return ""
     try:
-        return subprocess.check_output(ldap_cmd).decode("utf-8")
+        return subprocess.check_output(ldap_cmd, stderr=subprocess.DEVNULL).decode("utf-8")
     except subprocess.CalledProcessError as e:
         if e.returncode == 4:
+            print("--")
+            print("too-many-results")
+            print("--")
             sys.exit(4)
         raise e
 
@@ -106,21 +133,25 @@ def print_results_for_vim(ldap_results, extra_fields):
     for result in ldap_results:
         print("{} <{}>".format(result[FIELD_CN], result[FIELD_MAIL]))
 
-
 def main(argv):
     args = parse_args_or_exit(argv)
     fields = REQUIRED_FIELDS + args.extra_fields + [args.sortby]
     fields = list(set(fields))
     if args.mock_input:
-        ldap_results = read_mock_file(args.mock_input)
+        ldap_results_people, ldap_results_groups = read_mock_file(args.mock_input), None
     else:
-        ldap_results = ldapsearch(args.host, args.base, args.sortby,
-                                  args.filter, fields, args.dry_run)
-    results = process_results(ldap_results)
+        ldap_results_people = ldapsearch(args.host, args.base_people, args.sortby,
+                                         args.filter, fields, args.dry_run)
+        ldap_results_groups = ldapsearch(args.host, args.base_groups, args.sortby,
+                                         args.filter, fields, args.dry_run)
+    results_people = process_results(ldap_results_people)
+    results_groups = process_results(ldap_results_groups)
     if args.format == ARG_FORMAT_MUTT:
-        print_results_for_mutt(results, args.extra_fields)
+        printer = print_results_for_mutt
     elif args.format == ARG_FORMAT_VIM:
-        print_results_for_vim(results, args.extra_fields)
+        printer = print_results_for_vim
+
+    printer(results_people, args.extra_fields)
 
 
 if __name__ == "__main__":
